af3c85a9deee1ec0a30d5b0d9e7cbd274e841a60
Asif Ahmed <asif@stitchdesignco.com>
2017-06-28 02:50:48 -0400

    Add styles to login section of checkout

--- content/themes/storyrugs/woocommerce/content-product/co-content.php
+++ content/themes/storyrugs/woocommerce/content-product/co-content.php
@@ -5,15 +5,17 @@
 
 ?>
 
-<div class="co-head">
-  <?php include(locate_template('woocommerce/content-product/co-head.php')); ?>
-</div>
-<div class="co-attributes">
-	<?php include(locate_template('woocommerce/content-product/co-attributes.php')); ?>
-</div>
-<div class="co-cart">
-	<?php include(locate_template('woocommerce/content-product/co-cart.php')); ?>
-</div>
-<div class="co-share">
-	<?php include(locate_template('woocommerce/content-product/co-share.php')); ?>
+<div class="content-floater">
+	<div class="co-head">
+	  <?php include(locate_template('woocommerce/content-product/co-head.php')); ?>
+	</div>
+	<div class="co-attributes">
+		<?php include(locate_template('woocommerce/content-product/co-attributes.php')); ?>
+	</div>
+	<div class="co-cart">
+		<?php include(locate_template('woocommerce/content-product/co-cart.php')); ?>
+	</div>
+	<div class="co-share">
+		<?php include(locate_template('woocommerce/content-product/co-share.php')); ?>
+	</div>
 </div>
